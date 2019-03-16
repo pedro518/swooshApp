726714babf6cf7b69c981e24fe4043a92c9579b4
Pedro Antonio Ruiz Cuesta <pruiz@Pedros-iMac.local>
2019-03-16 04:14:41 -0700

    Intro to auto layout

---
+++ SwooshApp/SwooshApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwooshApp
+//
+//  Created by Pedro Antonio Ruiz Cuesta on 16/03/2019.
+//  Copyright © 2019 Pedro Antonio Ruiz Cuesta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
