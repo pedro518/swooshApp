5e14dfa7c91a8a876b3a47ad2479bf11ebe4d546
Pedro Antonio Ruiz Cuesta <pruiz@Pedros-iMac.local>
2019-03-17 04:37:10 -0700

    Se cambia el nombre del VC principal y se crean VC para cada Screen

---
+++ SwooshApp/SwooshApp/controller/LeagueVC.swift
@@ -0,0 +1,30 @@
+//
+//  LeagueVCViewController.swift
+//  SwooshApp
+//
+//  Created by Pedro Antonio Ruiz Cuesta on 17/03/2019.
+//  Copyright © 2019 Pedro Antonio Ruiz Cuesta. All rights reserved.
+//
+
+import UIKit
+
+class LeagueVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
