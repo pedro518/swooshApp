ede75c79b3db64d63cddddb0a07df76a9947940d
Pedro Antonio Ruiz Cuesta <pruiz@Pedros-iMac.local>
2019-03-18 13:14:07 -0700

    Se pasa info de player entre LeagueVC y SkillVC

--- SwooshApp/SwooshApp/controller/SkillVC.swift
+++ SwooshApp/SwooshApp/controller/SkillVC.swift
@@ -9,11 +9,15 @@
 import UIKit
 
 class SkillVC: UIViewController {
+    
+    var player : Player!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        if let desiredLeague = player.desiredLeague {
+            print(desiredLeague)
+        }   
     }
     
 
